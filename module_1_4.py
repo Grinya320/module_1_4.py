9b75ad3e9a7ca04b1d826022a022cf434b0cb45b
Grinya320 <grilya320@gmail.com>
2024-10-16 14:47:03 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('какое у тебя хобби? ')
+print('в предложении',my_string,len(my_string),'букв')
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string [0])
+print(my_string [-1])
